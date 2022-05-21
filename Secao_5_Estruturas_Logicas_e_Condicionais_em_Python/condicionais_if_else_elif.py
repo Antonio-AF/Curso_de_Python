b2f458b9c7ba97b8722e61e7d7e8e888f48fd26a
Antonio-AF <antonio.farias.002@gmail.com>
2022-05-21 12:47:48 -0300

    Ajustes na Seção 5

--- Secao_5_Estruturas_Logicas_e_Condicionais_em_Python/condicionais_if_else_elif.py
+++ Secao_5_Estruturas_Logicas_e_Condicionais_em_Python/condicionais_if_else_elif.py
@@ -1,23 +1,10 @@
-"""
-Estruturas Condicionais
-
-if (Se), else, elif.
-
-
-# Estrutura condicional if, em C.
-
-if(idade < 18){
-    printf("Menor de idade");
-}
-"""
-
 idade = int(input('Qual a sua idade?'))
 
 if idade < 12:
     print('Você é criança:')
 
 elif (idade >= 12) and (idade <= 18):
-    print('Você é adolecente:')
+    print('Você é adolescente:')
 
 else:
     print("Maior de idade:")
